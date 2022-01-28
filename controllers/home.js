a03f3c594aead3d57c0b097af57fdaec9699d1db
Lee Morgan <me@leemorgan.io>
2022-01-28 17:21:57 +0300

    Add sudoku solver.

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,9 @@
 module.exports = {
     landing: function(req, res){
         return res.render("landing/landing.eta");
+    },
+
+    sudoku: function(req, res){
+        return res.render("sudoku/sudoku.eta");
     }
 }
--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ const home = require("./controllers/home.js");
 module.exports = (app)=>{
     //HOME
     app.get("/", home.landing);
+    app.get("/sudoku", home.sudoku);
 }
